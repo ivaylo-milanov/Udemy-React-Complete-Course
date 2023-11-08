d8cb7dadf1077e544cd491a3a8e5e84ac41db672
ivaylo-milanov <imilanov24@gmail.com>
2023-11-08 13:41:09 +0200

    Added state to the players

--- tic-tac-toe-starting-project/src/components/Player.jsx
+++ tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,27 @@
-export default function Player({ name, symbol }) {
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(editing => !editing);
+  }
+
+  const btnCaption = isEditing ? 'Save' : 'Edit';
+
+  function handleNameChange(event) {
+    setPlayerName(event.target.value);
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {isEditing && <input type="text" required value={playerName} onChange={handleNameChange}/>}
+        {!isEditing && <span className="player-name">{playerName}</span>}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{btnCaption}</button>
     </li>
   );
 }
